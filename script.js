691c5d8808da31397315612724d02bf453fe4b5f
aashish-0 <aaashishg92@gmail.com>
2024-07-29 18:04:09 +0530

    Pushing some changes like weather icon update on every search

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apikey = "77352dabd3a8d0efaa23bdc28d776865";
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city) {
     try {
@@ -13,6 +14,24 @@ async function checkWeather(city) {
             document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
             document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
             document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+            if(data.weather[0].main == "Clouds"){
+                weatherIcon.src = "./images/clouds.png";
+            }
+            else if(data.weather[0].main == "Clear"){
+                weatherIcon.src = "./images/clear.png";
+            }
+            else if(data.weather[0].main == "Rain"){
+                weatherIcon.src = "./images/rain.png";
+            }
+            else if(data.weather[0].main == "Drizzle"){
+                weatherIcon.src = "./images/drizzle.png";
+            }
+            else if(data.weather[0].main == "Mist"){
+                weatherIcon.src = "./images/mist.png";
+            }
+
+            document.querySelector(".weather").style.display = "block";
         } else {
             console.error("Error: ", data.message);
             alert("City not found!");
